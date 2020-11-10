18f42100ba182e41fdc46a136edaf2d9ea13f3b5
Florian Dejonckheere <florian@floriandejonckheere.be>
2020-11-10 13:21:58 +0100

    Add thalarion cloud instance

---
+++ terraform/thalarion/master.tf
@@ -0,0 +1,93 @@
+locals {
+  cloud_init = templatefile("master.tmpl.yml", {
+    ip_address = hcloud_floating_ip.master.ip_address,
+    ssh_public_key = file(var.ssh_public_key),
+    sshd_public_key = file(var.sshd_public_key),
+    sshd_private_key = file(var.sshd_private_key),
+    domain_name = var.domain_name,
+    hostname = var.hostname,
+    passwd = var.passwd,
+    storage_box_url: var.storage_box_url,
+  })
+}
+
+output "cloud_init" {
+  value = local.cloud_init
+}
+
+provider "hcloud" {
+  token = var.hcloud_token
+}
+
+resource "hcloud_ssh_key" "master" {
+  name = var.hostname
+  public_key = file(var.ssh_public_key)
+}
+
+resource "hcloud_server" "master" {
+  name = var.hostname
+  location = "fsn1"
+  image = "debian-10"
+  server_type = "cx11"
+  ssh_keys = [hcloud_ssh_key.master.id]
+  user_data = local.cloud_init
+  backups = false
+
+//  provisioner "remote-exec" {
+//    connection {
+//      host = self.ipv4_address
+//      type = "ssh"
+//      user = "root"
+//      private_key = file(var.ssh_private_key)
+//    }
+//
+//    inline = [
+//      "echo 'auto eth0:1\niface eth0:1 inet static\n  address ${hcloud_floating_ip.master.ip_address}/24\n' > /etc/network/interfaces.d/60-floating-ip.cfg",
+//      "ifdown eth0; ifup eth0",
+//    ]
+//  }
+}
+
+resource "hcloud_rdns" "master4" {
+  server_id = hcloud_server.master.id
+  ip_address = hcloud_server.master.ipv4_address
+  dns_ptr = var.domain_name
+}
+
+resource "hcloud_rdns" "master6" {
+  server_id = hcloud_server.master.id
+  ip_address = hcloud_server.master.ipv6_address
+  dns_ptr = var.domain_name
+}
+
+resource "hcloud_network" "master" {
+  name = "master"
+  ip_range = "10.0.0.0/8"
+}
+
+resource "hcloud_network_subnet" "master" {
+  network_id = hcloud_network.master.id
+  type = "server"
+  network_zone = "eu-central"
+  ip_range = "10.0.1.0/24"
+}
+
+resource "hcloud_floating_ip" "master" {
+  name = "master"
+  type = "ipv4"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "hcloud_floating_ip_assignment" "master" {
+  floating_ip_id = hcloud_floating_ip.master.id
+  server_id = hcloud_server.master.id
+}
+
+resource "hcloud_rdns" "floating_master" {
+  floating_ip_id = hcloud_floating_ip.master.id
+  ip_address = hcloud_floating_ip.master.ip_address
+  dns_ptr = var.domain_name
+}
---
+++ terraform/thalarion/variables.tf
@@ -0,0 +1,43 @@
+variable "hcloud_token" {
+  type = string
+}
+
+variable "passwd" {
+  description = "UNIX password hash for cloud user"
+  type = string
+}
+
+variable "domain_name" {
+  description = "Web-facing domain name"
+  type = string
+}
+
+variable "hostname" {
+  description = "Machine hostname"
+  type = string
+}
+
+variable "ssh_public_key" {
+  description = "Path to SSH public key"
+  type = string
+}
+
+variable "ssh_private_key" {
+  description = "Path to SSH private key"
+  type = string
+}
+
+variable "sshd_public_key" {
+  description = "Path to SSH server public key"
+  type = string
+}
+
+variable "sshd_private_key" {
+  description = "Path to SSH server private key"
+  type = string
+}
+
+variable "storage_box_url" {
+  description = "URL to storage box share"
+  type = string
+}
