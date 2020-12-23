fcaaeb16d2d7727b4526eb159aca230539f6e277
Florian Dejonckheere <florian@floriandejonckheere.be>
2020-12-23 18:52:12 +0100

    Reenable floriandejonckheere.be

--- terraform/domains.tf
+++ terraform/domains.tf
@@ -13,17 +13,17 @@ module "dejonckhee_re" {
   gsf = lookup(var.gsf, "dejonckhee_re")
 }
 
-//module "floriandejonckheere_be" {
-//  source = "./modules/dns/domain"
-//
-//  zone = "floriandejonckheere.be"
-//  # FIXME: uncomment when cloud migration is complete
-//  ipv4_address = "168.119.155.79" #module.web.public_ipv4
-//  domain_contact = var.domain_contact
-//  domain_contact_extra = var.domain_contact_extra
-//  dkim = lookup(var.dkim, "floriandejonckheere_be")
-//  gsf = lookup(var.gsf, "floriandejonckheere_be")
-//}
+module "floriandejonckheere_be" {
+  source = "./modules/dns/domain"
+
+  zone = "floriandejonckheere.be"
+  # FIXME: uncomment when cloud migration is complete
+  ipv4_address = "168.119.155.79" #module.web.public_ipv4
+  domain_contact = var.domain_contact
+  domain_contact_extra = var.domain_contact_extra
+  dkim = lookup(var.dkim, "floriandejonckheere_be")
+  gsf = lookup(var.gsf, "floriandejonckheere_be")
+}
 
 module "headbang_re" {
   source = "./modules/dns/domain"
