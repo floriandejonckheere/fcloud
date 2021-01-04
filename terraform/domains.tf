df48cea1c54a694eebc790664373e72bb872fc79
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-01-04 20:00:18 +0100

    Enable cloud root DNS records

--- terraform/domains.tf
+++ terraform/domains.tf
@@ -5,8 +5,7 @@ module "dejonckhee_re" {
   source = "./modules/dns/domain"
 
   zone = "dejonckhee.re"
-  # FIXME: uncomment when cloud migration is complete
-  ipv4_address = "168.119.155.79" #module.web.public_ipv4
+  ipv4_address = module.web.public_ipv4
   domain_contact = var.domain_contact
   domain_contact_extra = var.domain_contact_extra
   dkim = lookup(var.dkim, "dejonckhee_re")
@@ -17,8 +16,7 @@ module "floriandejonckheere_be" {
   source = "./modules/dns/domain"
 
   zone = "floriandejonckheere.be"
-  # FIXME: uncomment when cloud migration is complete
-  ipv4_address = "168.119.155.79" #module.web.public_ipv4
+  ipv4_address = module.web.public_ipv4
   domain_contact = var.domain_contact
   domain_contact_extra = var.domain_contact_extra
   dkim = lookup(var.dkim, "floriandejonckheere_be")
@@ -29,8 +27,7 @@ module "headbang_re" {
   source = "./modules/dns/domain"
 
   zone = "headbang.re"
-  # FIXME: uncomment when cloud migration is complete
-  ipv4_address = "168.119.155.79" #module.web.public_ipv4
+  ipv4_address = module.web.public_ipv4
   domain_contact = var.domain_contact
   domain_contact_extra = var.domain_contact_extra
   dkim = lookup(var.dkim, "headbang_re")
