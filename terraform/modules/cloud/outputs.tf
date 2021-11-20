9fb2e11623f53ff40803008c5ddc8f9914b6205a
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-11-20 14:26:48 +0100

    Add cloud module

---
+++ terraform/modules/cloud/outputs.tf
@@ -0,0 +1,19 @@
+output "network_id" {
+  value = hcloud_network.default.id
+}
+
+output "network_ipv4" {
+  value = hcloud_server_network.default.ip
+}
+
+output "private_ipv4" {
+  value = hcloud_server.default.ipv4_address
+}
+
+output "private_ipv6" {
+  value = hcloud_server.default.ipv6_address
+}
+
+output "public_ipv4" {
+  value = hcloud_floating_ip.default.ip_address
+}
