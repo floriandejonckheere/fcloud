c7a7d4b56fadd666e27d68ec25824d07f5eff941
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-12-21 18:57:52 +0100

    Add cloud server

--- terraform/services.tf
+++ terraform/services.tf
@@ -1,6 +1,34 @@
 ##
 # Services
 #
+module "cloud" {
+  source = "./modules/cloud"
+
+  name = "cloud"
+  zone = "dejonckhee.re"
+  passwd = var.passwd
+  ssh_public_key = "~/.ssh/cloud.pub"
+  ssh_private_key = "~/.ssh/cloud"
+  sshd_public_key = "~/.ssh/cloud-sshd.pub"
+  sshd_private_key = "~/.ssh/cloud-sshd"
+
+  # Type    vCPU  RAM   Price/h  Price/mo
+  # cx11    1     2G    0.007/h   4.22/mo
+  # cpx11   2     2G    0.008/h   4.83/mo
+  # cx21    2     4G    0.010/h   5.93/mo
+  # cpx21   3     4G    0.013/h   8.35/mo
+  # cx31    2     8G    0.017/h  10.77/mo
+  # cpx31   4     8G    0.024/h  15.00/mo
+  # cx41    4    16G    0.031/h  19.24/mo
+  # cpx41   8    16G    0.046/h  27.71/mo
+  # cx51    8    32G    0.061/h  36.18/mo
+  # cpx51   16   32G    0.097/h  60.38/mo
+  server_type = "cx11"
+}
+
+##
+# Services
+#
 module "web" {
   source = "./modules/web"
 
