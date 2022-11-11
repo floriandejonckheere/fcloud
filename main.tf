7d61a185b1b81311f8f091becf513eaa14ab8f63
Florian Dejonckheere <florian@floriandejonckheere.be>
2022-11-11 15:44:02 +0200

    Drop vpn module and scaleway config

--- main.tf
+++ main.tf
@@ -14,14 +14,6 @@ provider "hcloud" {
   token = var.hcloud_token
 }
 
-provider "scaleway" {
-  access_key = var.scaleway_access_key
-  secret_key = var.scaleway_secret_key
-  organization_id = var.scaleway_organization_id
-  zone = "nl-ams-1"
-  region = "nl-ams"
-}
-
 provider "gandi" {
   key = var.gandi_key
 }
--- variables.tf
+++ variables.tf
@@ -5,17 +5,6 @@ variable "hcloud_token" {
   type = string
 }
 
-variable "scaleway_access_key" {
-  type = string
-}
-variable "scaleway_secret_key" {
-  type = string
-}
-
-variable "scaleway_organization_id" {
-  type = string
-}
-
 variable "gandi_key" {
   type = string
 }
