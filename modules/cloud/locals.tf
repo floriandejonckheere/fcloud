f1ece19c0323ed959dc63c3ec1f68b34a1890df0
Florian Dejonckheere <florian@floriandejonckheere.be>
2022-11-23 09:16:38 +0200

    Use DHCP for eth0 interface

--- modules/cloud/locals.tf
+++ modules/cloud/locals.tf
@@ -1,7 +1,8 @@
 locals {
   fqdn = "${var.name}.${var.zone}"
   cloud_init = templatefile("${path.module}/default.tmpl.yml", {
-    ip_address = hcloud_primary_ip.default4.ip_address,
+    ip4_address = hcloud_primary_ip.default4.ip_address,
+    ip6_address = hcloud_primary_ip.default6.ip_address,
     ssh_public_key = file(var.ssh_public_key),
     sshd_public_key = file(var.sshd_public_key),
     sshd_private_key = file(var.sshd_private_key),
