70096c63292c1bd773a1828569f4d7993f5085c0
Florian Dejonckheere <florian@floriandejonckheere.be>
2023-09-06 21:48:10 +0200

    Move provider configuration block

--- main.tf
+++ main.tf
@@ -1,10 +1,24 @@
 ##
-# Backend
+# Terraform configuration
 #
 terraform {
   backend "local" {
     path = "terraform.tfstate"
   }
+
+  required_providers {
+    gandi = {
+      source = "psychopenguin/gandi"
+      version = "2.0.0-rc3"
+    }
+    hcloud = {
+      source = "hetznercloud/hcloud"
+    }
+    scaleway = {
+      source = "scaleway/scaleway"
+    }
+  }
+  required_version = ">= 0.13"
 }
 
 ##
